54325617126f756b966dc2bd0e7871d80d3090ee
Dewang Kumar Agarwal <agdewang@outlook.com>
2023-04-13 23:43:32 -0400

    Update Step4.cu

--- n4GPU/Step4.cu
+++ n4GPU/Step4.cu
@@ -7,16 +7,26 @@ __global__ void alternatingGraphPathSearch(int32_t* coveredRows, int32_t* covere
     volatile int32_t* coveredCols1 = coveredCols;
     volatile int32_t* coveredRows1 = coveredRows;
 
-    __shared__ int32_t zeroFound, kernelRepeat, goToStep5_shared;
+    __shared__ int32_t zeroFound, goToStep5_shared;
+    int32_t kernelRepeat = 0;
 
     int32_t thread = threadIdx.x;
     int32_t block = blockIdx.x;
+    int32_t blockSize = blockDim.x;
     int32_t numberOfZeros = zeroesSizes[block];
+    int32_t numberOfZeroesForThisThread = numberOfZeros / blockSize;
+    int32_t numberOfZerosUnderMultiple = numberOfZeros % blockSize;
+    int32_t startingIndex = 0;
+    if (thread < numberOfZerosUnderMultiple) {
+        numberOfZeroesForThisThread++;
+        startingIndex = thread * numberOfZeroesForThisThread;
+    } else {
+        startingIndex = numberOfZerosUnderMultiple * (numberOfZeroesForThisThread + 1) + (thread - numberOfZerosUnderMultiple) * numberOfZeroesForThisThread;
+    }
 
     int32_t zero = -1, zeroRow = -1, zeroCol = -1, starCol = -1;
 
     if (thread == 0) {
-        kernelRepeat = 0;
         goToStep5_shared = 0;
     }
 
@@ -25,8 +35,8 @@ __global__ void alternatingGraphPathSearch(int32_t* coveredRows, int32_t* covere
         if (thread == 0) zeroFound = 0;
         __syncthreads();
 
-        for (int32_t i = thread; i < numberOfZeros; i += blockDim.x) {
-            zero = zeroes[block * numberOfDataPerBlock + i];
+        for (int32_t i = 0; i < numberOfZeroesForThisThread && startingIndex + i < numberOfZeros; ++i) {
+            zero = zeroes[block * numberOfDataPerBlock + startingIndex + i];
             zeroRow = zero % workers;
             zeroCol = zero / workers;
             starCol = colOfStarAtRow[zeroRow];
@@ -47,8 +57,7 @@ __global__ void alternatingGraphPathSearch(int32_t* coveredRows, int32_t* covere
     } while(zeroFound == 1 && goToStep5_shared == 0);
 
     if (thread == 0) {
-        if (kernelRepeat) *repeatKernel = 1;
-	    if (goToStep5_shared) *goToStep5 = 1;
+        if (goToStep5_shared) *goToStep5 = 1;
     }
-
+    if (kernelRepeat) *repeatKernel = 1;
 }
