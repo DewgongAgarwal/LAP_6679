0746dbcc7c6d0f262346580cf36ad7cdb2396915
Dewang Kumar Agarwal <agdewang@outlook.com>
2023-04-13 23:44:02 -0400

    Update Step2.cu

--- n4GPU/Step2.cu
+++ n4GPU/Step2.cu
@@ -5,26 +5,29 @@
 
 __global__ void initialMatching(int32_t *rowOfStarAtCol, int32_t *colOfStarAtRow, int32_t *coveredRows, int32_t *coveredCols, int32_t *zeroes, int32_t *zeroesSizes, int32_t* repeatKernel, int32_t numberOfDataPerBlock, int32_t workers) {
 
-    __shared__ int32_t repeat, kernelRepeat;
+    int32_t repeat = 0, kernelRepeat = 0;
     int32_t thread = threadIdx.x;
     int32_t block = blockIdx.x;
     int32_t blockSize = blockDim.x;
 
-    if (thread == 0) {
-        kernelRepeat = 0;
-    }
-
     int32_t numberOfZeros = zeroesSizes[block];
+    int32_t numberOfZeroesForThisThread = numberOfZeros / blockSize;
+    int32_t numberOfZerosUnderMultiple = numberOfZeros % blockSize;
+    int32_t startingIndex = 0;
+    if (thread < numberOfZerosUnderMultiple) {
+        numberOfZeroesForThisThread++;
+        startingIndex = thread * numberOfZeroesForThisThread;
+    } else {
+        startingIndex = numberOfZerosUnderMultiple * (numberOfZeroesForThisThread + 1) + (thread - numberOfZerosUnderMultiple) * numberOfZeroesForThisThread;
+    }
 
     int32_t zero = -1, zeroRow = -1, zeroCol = -1;
 
     
     do {
-        __syncthreads();
-        if (thread == 0) repeat = 0;
-        __syncthreads();
-        for (int32_t i = thread; i < numberOfZeros; i += blockSize) {
-            zero = zeroes[block * numberOfDataPerBlock + i];
+        repeat = 0;
+        for (int32_t i = 0; i < numberOfZeroesForThisThread && startingIndex + i < numberOfZeros; ++i) {
+            zero = zeroes[block * numberOfDataPerBlock + startingIndex + i];
             zeroRow = zero % workers;
             zeroCol = zero / workers;
             if(coveredRows[zeroRow] == 0 && coveredCols[zeroCol] == 0) {
